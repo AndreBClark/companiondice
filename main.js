63928fbd9cc39c082adcb82c4a158b1c8a4a2f49
Andre Clark <andre@cosmicdivision.dev>
2020-01-03 13:37:40 -0800

    :truck:  add ids && move logic to js file
    Co-Authored-By: Nathan Goullette <nathangoullette@users.noreply.github.com>

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ function RNG(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+const x = document.getElementById("dTwentyBtn");
+const y = document.getElementById("dTwentyOutPut");
+
+x.addEventListener("click", RespondNumbs);
+}
