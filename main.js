a6413884e5141c2ab87b367b6ee8d679cacf97b8
Andre Clark <andre@cosmicdivision.dev>
2020-01-03 13:38:44 -0800

    :sparkles: add slot machine animation
    from https://codepen.io/hankthewhale/pen/NwKvLy
    
    Co-Authored-By: Nathan Goullette <nathangoullette@users.noreply.github.com>

--- main.js
+++ main.js
@@ -8,4 +8,35 @@ const x = document.getElementById("dTwentyBtn");
 const y = document.getElementById("dTwentyOutPut");
 
 x.addEventListener("click", RespondNumbs);
+
+function RespondNumbs() {
+    let start = 1;
+    let end = RNG(1, 21);
+    let ticks = 20;
+    let speed = 60;
+
+    y.innerHTML = end;
+
+    let randomNumbers = [end]
+
+    for (let i = 0; i < ticks - 1; i++) {
+        randomNumbers.unshift(
+            Math.floor(Math.random() * (end - start + 1) + start)
+        );
+    }
+
+    randomNumbers.sort((a, b) => { return a - b });
+
+    console.log(randomNumbers.length)
+
+    let z = 0;
+    let interval = setInterval(function() {
+
+        y.innerHTML = randomNumbers.shift();
+
+        if (++z === ticks) {
+            window.clearInterval(interval);
+        }
+
+    }, speed);
 }
