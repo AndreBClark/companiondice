d026ff525cf7fb6fb7b6a2e8bc2667f274300ec7
AndreBClark <andre@cosmicdivision.dev>
2020-01-06 23:26:30 -0800

    :recycle: add die variation functions

--- src/main.js
+++ src/main.js
@@ -6,17 +6,32 @@ const d20Display = document.getElementById("dTwentyOutput");
 d20.addEventListener("click", function() {
     rollNSidedDie(20)
 })
+d12.addEventListener("click", function() {
+    rollNSidedDie(12)
+})
+d10.addEventListener("click", function() {
+    rollNSidedDie(10)
+})
+d8.addEventListener("click", function() {
+    rollNSidedDie(8)
+})
+d6.addEventListener("click", function() {
+    rollNSidedDie(6)
+})
+d4.addEventListener("click", function() {
+    rollNSidedDie(4)
+})
 
 function rollNSidedDie(sides) {
     let speed = 60;
     let result = randomNumGen(sides);
-    let ticks = randomNumGen(10) + 9;
+    let ticks = randomNumGen(10) + 10;
 
     d20Display.innerHTML = result;
 
     let randomArray = [result]
 
-    for (let i = 0; i < ticks - 1; i++) {
+    for (let i = 0; i < ticks; i++) {
         randomArray.unshift(
             randomNumGen(sides)
         );
