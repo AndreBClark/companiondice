d66e2e37a4130e3eb437e3b5e7dc53b4e4c7543f
AndreBClark <andre@cosmicdivision.dev>
2020-01-09 19:59:41 -0800

    :alembic: add hook state with clock

--- src/dice.jsx
+++ src/dice.jsx
@@ -1,19 +1,28 @@
 import { h,render } from 'preact';
 import { Link } from '@reach/router';
-
+import { useState,useEffect } from 'preact/hooks';
 
 
 const Dice = props => {
+    const [date, setDate] = useState(new Date());
+    useEffect(() => {
+      var timerID = setInterval(() => tick(), 1000);
+      return function cleanup() {
+        clearInterval(timerID);
+      };
+    });
+    const tick = () => setDate(new Date());
     return (
         <div class="container mx-auto">
             <button id="dTwentyBtn" class="btn-d-twenty">
             <p id="dTwentyOutput" class="text-blue-400 font-bold m-4 my-auto mx-auto my-auto text-4xl">
-                {props.sides}
+                {props.sides} 
             </p>
+                <h3>{date.toLocaleTimeString()}</h3>
             </button>
             <Link class="text-gray-300 mx-auto text-center" to="/">Home</Link>
         </div>
-        )
+        ) 
 }
 
 export default Dice;
