316e69f653f3b4297c7ff5ed95e0abbfb2100ee1
AndreBClark <andre@cosmicdivision.dev>
2020-01-09 22:18:16 -0800

    :recycle: refactor rollNSidedDie function

--- src/main.js
+++ src/main.js
@@ -3,25 +3,6 @@ randomNumGen = (max) => Math.ceil(Math.random() * max)
 const d20 = document.getElementById("dTwentyBtn");
 const d20Display = document.getElementById("dTwentyOutput");
 
-d20.addEventListener("click", function() {
-    rollNSidedDie(20)
-})
-d12.addEventListener("click", function() {
-    rollNSidedDie(12)
-})
-d10.addEventListener("click", function() {
-    rollNSidedDie(10)
-})
-d8.addEventListener("click", function() {
-    rollNSidedDie(8)
-})
-d6.addEventListener("click", function() {
-    rollNSidedDie(6)
-})
-d4.addEventListener("click", function() {
-    rollNSidedDie(4)
-})
-
 function rollNSidedDie(sides) {
     let speed = 60;
     let result = randomNumGen(sides);
@@ -39,12 +20,9 @@ function rollNSidedDie(sides) {
 
     let displayRefresh = 0;
     let interval = setInterval(function() {
-
         d20Display.innerHTML = randomArray.shift();
-
         if (++displayRefresh === ticks) {
             window.clearInterval(interval);
         }
-
     }, speed);
 }
