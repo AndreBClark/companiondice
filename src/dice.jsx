03ca71c33c2b2f6744b3810cd2d7c1ac69c03981
AndreBClark <andre@cosmicdivision.dev>
2020-01-09 19:36:30 -0800

    :alembic: pass url params to props

--- src/dice.jsx
+++ src/dice.jsx
@@ -3,42 +3,12 @@ import { Link } from '@reach/router';
 
 
 
-const Dice = () => {
-    const sides = 20;
-    const d20 = document.getElementById("dTwentyBtn");
-    let d20Display = document.getElementById("dTwentyOutput");
-    const randomNumGen = (max) => Math.ceil(Math.random() * max);
-    function rollNSidedDie(sides) {
-        let speed = 60;
-        let result = randomNumGen(sides);
-        let ticks = randomNumGen(10) + 10;
-    
-        d20Display.innerHTML = result;
-    
-        let randomArray = [result]
-    
-        for (let i = 0; i < ticks; i++) {
-            randomArray.unshift(
-                randomNumGen(sides)
-            );
-        }
-    
-        let displayRefresh = 0;
-        let interval = setInterval(function() {
-    
-            d20Display.innerHTML = randomArray.shift();
-    
-            if (++displayRefresh === ticks) {
-                window.clearInterval(interval);
-            }
-    
-        }, speed);
-    }
+const Dice = props => {
     return (
         <div class="container mx-auto">
-            <button onClick={() => rollNSidedDie(20)} id="dTwentyBtn" class="btn-d-twenty">
+            <button id="dTwentyBtn" class="btn-d-twenty">
             <p id="dTwentyOutput" class="text-blue-400 font-bold m-4 my-auto mx-auto my-auto text-4xl">
-                {d20Display}
+                {props.sides}
             </p>
             </button>
             <Link class="text-gray-300 mx-auto text-center" to="/">Home</Link>
--- src/index.jsx
+++ src/index.jsx
@@ -4,22 +4,41 @@ import Dice from './dice'
 if (module.hot) {
     module.hot.accept()
 }
-const App = ({children}) => (
-    <div class="container mx-auto bg-gray-800 flex justify-between flex-1">
+class Sides extends Component {
+    render() {
+      return (
+        <span>
+          {this.props.value}
+        </span>
+      );
+    }
+  }
+  class Board extends Component {
+
+  }
+
+class App extends Component {
+    renderSides(i) {
+        return <Sides value={i} />;
+      }
+render() {
+    return (
+        <div class="container mx-auto bg-gray-800 flex justify-between flex-1">
         <button class="btn d4">D4</button>
         <button class="btn d6">D6</button>
         <button class="btn d8">D8</button>
         <button class="btn d10">D10</button>
         <button class="btn d12">D12</button>
-        <button class="btn d20">D20</button>
-        <Link to="dice">Dice</Link>
-        {children}
+    <Link class="btn d20" to="dice/20">D{this.renderSides(20)}</Link>
     </div>
-);
+        )
+    }
+}
 
 render(
     <Router>
         <App path="/" exact />
-        <Dice path="dice"/>
+        <Dice path="dice" exact/>
+        <Dice path="dice/:sides"/>
     </Router>
     , document.body);
