fab62e3301b187eb2d6b06150eeaf96353a6fcd3
AndreBClark <andre@cosmicdivision.dev>
2020-01-15 22:21:26 -0800

    :recycle: refactor and Class-ify handleClick()

--- src/dice.jsx
+++ src/dice.jsx
@@ -5,17 +5,19 @@ export const randomNumGen = (max) => Math.ceil(Math.random() * max);
 class Dice extends Component {
     constructor(props) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
+        };
     }
-    function handleClick(e) {
         const result = randomNumGen(props.sides);
         console.log(result);
+    handleClick() {
     }
-        <div class="container mx-auto">
-            <button onClick={handleClick} 
             class="btn-d-twenty text-blue-400 py-auto font-bold m-4 my-auto          text-4xl">
                     <span class="m-auto">{props.sides}</span>
     render() {
         return (
+            <div class="container mx-auto">
+            <button onClick={this.handleClick} 
             </button>
             <Link to="/"
                 class="text-gray-300 font-bold mx-auto text-center block bg-blue-800 p-5">
