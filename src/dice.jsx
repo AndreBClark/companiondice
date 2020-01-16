d580eb60dec9c44fa014fcdf205c0128b5b92a62
AndreBClark <andre@cosmicdivision.dev>
2020-01-15 22:11:48 -0800

    :recycle: switch to Class component

--- src/dice.jsx
+++ src/dice.jsx
@@ -1,20 +1,21 @@
-import { h,render } from 'preact';
+import { h,render, Component } from 'preact';
 import { Link } from '@reach/router';
 import { useState,useEffect } from 'preact/hooks';
-
-function Dice(props) {
-    function randomNumGen(max) {
          return Math.ceil(Math.random() * max)
+class Dice extends Component {
+    constructor(props) {
+        super(props);
     }
     function handleClick(e) {
         const result = randomNumGen(props.sides);
         console.log(result);
     }
-    return (
         <div class="container mx-auto">
             <button onClick={handleClick} 
             class="btn-d-twenty text-blue-400 py-auto font-bold m-4 my-auto          text-4xl">
                     <span class="m-auto">{props.sides}</span>
+    render() {
+        return (
             </button>
             <Link to="/"
                 class="text-gray-300 font-bold mx-auto text-center block bg-blue-800 p-5">
@@ -22,6 +23,7 @@ function Dice(props) {
             </Link>
         </div>
         ) 
+    }
 }
 
 export default Dice;
