0e6df8d4469d2e0e39306bd3febdb90a9fab7bcb
AndreBClark <andre@cosmicdivision.dev>
2020-01-22 21:41:16 -0800

    :wrench: configure cssnano and purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,16 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: [
+        './src/**/*.jsx',
+    ],
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+const cssnano = require('cssnano')('cssnane-advanced-preset');
+
 module.exports = {
     "plugins": [
         require('stylelint')('./stylelint.config.js'),
         require('tailwindcss'),
         require('autoprefixer'),
+        ...process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []
     ]
 }
