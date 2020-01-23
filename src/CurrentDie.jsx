dd9161eff1c51787117135ee6e82ee819505aa8b
AndreBClark <andre@cosmicdivision.dev>
2020-01-22 23:08:58 -0800

    :sparkles: improve dice roll math simulation

--- src/CurrentDie.jsx
+++ src/CurrentDie.jsx
@@ -7,7 +7,7 @@ import D10 from './assets/d10.svg';
 import D12 from './assets/d12.svg';
 import D20 from './assets/d20.svg';
 
-export const randomNumGen = (min, max) => Math.ceil(Math.random() * (max - min));
+export const randomNumGen = (min, max) => Math.ceil(Math.random() * ((max - min) + min));
 export default class CurrentDie extends Component {
     constructor(props) {
         super(props);
@@ -33,7 +33,7 @@ export default class CurrentDie extends Component {
         const randomArray = [this.state.result];
         for (let i = 0; i < this.state.ticks; i++) {
             randomArray.unshift(
-                randomNumGen(this.state.amount * this.props.sides, this.state.amount)
+                randomNumGen(this.state.amount, this.state.amount * this.props.sides)
             );
         }
         let displayRefresh = 0;
