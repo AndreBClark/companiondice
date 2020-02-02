075532acafb8b03b8e7b8807465edd65576582ea
AndreBClark <andre@cosmicdivision.dev>
2020-02-01 20:48:24 -0800

    :sunglasses: add deeppurple colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
+const tint = require('polished/lib/color/tint');
+const darken = require('polished/lib/color/darken');
 module.exports = {
     theme: {
+        extend: {
+            colors: {
+                deeppurple: {
+                    900: darken(.3, "#44337a"),
+                    800: darken(.25, "#44337a"),
+                    700: darken(.2, "#44337a"),
+                    600: darken(.175, "#44337a"),
+                    500: darken(.15, "#44337a"),
+                    400: darken(.125, "#44337a"),
+                    300: darken(.1, "#44337a"),
+                    200: darken(.05, "#44337a"),
+                    100: darken(.025, "#44337a"),
+                }
+            }
+        }
+    },
     variants: {},
     plugins: []
 }
