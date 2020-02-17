be2965c5fb0544e92a9457669129c5fe04abd67d
AndreBClark <andre@cosmicdivision.dev>
2020-02-16 16:40:08 -0800

    :truck: move dice to grouped component dice.jsx

---
+++ src/dice.jsx
@@ -0,0 +1,13 @@
+import d4 from './assets/d4.svg';
+import d6 from './assets/d6.svg';
+import d8 from './assets/d8.svg';
+import d10 from './assets/d10.svg';
+import d12 from './assets/d12.svg';
+import d20 from './assets/d20.svg';
+
+export const D4 = d4;
+export const D6 = d6;
+export const D8 = d8;
+export const D10 = d10;
+export const D12 = d12;
+export const D20 = d20;
