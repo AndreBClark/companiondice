223f88bae15fc33070b78994d3ee3d0024853725
AndreBClark <andre@cosmicdivision.dev>
2020-02-16 16:41:40 -0800

    :art: format SEO

--- src/SEO.jsx
+++ src/SEO.jsx
@@ -9,82 +9,82 @@ import PropTypes from 'prop-types';
 import Helmet from 'preact-helmet';
 
 const SEO = ({ description, lang, meta, keywords, title }) => {
-  const site = {
-    title: `Companion Dice`,
-    author: `Andre Clark`,
-    description: `Dice Rolling app for Playing Table Top Games`,
-    siteUrl: `https://companiondice.netlify.com`,
-    social: {
-        github: `AndreBClark`,
-    },
-}
+    const site = {
+        title: `Companion Dice`,
+        author: `Andre Clark`,
+        description: `Dice Rolling app for Playing Table Top Games`,
+        siteUrl: `https://companiondice.netlify.com`,
+        social: {
+            github: `AndreBClark`,
+        },
+    };
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      title={title}
-      titleTemplate={`%s | ${site.title}`}
-      meta={[
-        {
-          name: `description`,
-          content: site.description
-        },
-        {
-          property: `og:title`,
-          content: site.title,
-        },
-        {
-          property: `og:description`,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.author,
-        },
-        {
-          name: `twitter:title`,
-          content: site.title,
-        },
-        {
-          name: `twitter:description`,
-          content: site.description,
-        }
-      ]
-        .concat(
-          keywords.length > 0
-            ? {
-                name: `keywords`,
-                content: keywords.join(`, `),
-              }
-            : []
-        )
-        .concat(meta)}
-    />
-  )
-}
+    return (
+        <Helmet
+            htmlAttributes={{
+                lang,
+            }}
+            title={title}
+            titleTemplate={`%s | ${site.title}`}
+            meta={[
+                {
+                    name: `description`,
+                    content: site.description,
+                },
+                {
+                    property: `og:title`,
+                    content: site.title,
+                },
+                {
+                    property: `og:description`,
+                },
+                {
+                    property: `og:type`,
+                    content: `website`,
+                },
+                {
+                    name: `twitter:card`,
+                    content: `summary`,
+                },
+                {
+                    name: `twitter:creator`,
+                    content: site.author,
+                },
+                {
+                    name: `twitter:title`,
+                    content: site.title,
+                },
+                {
+                    name: `twitter:description`,
+                    content: site.description,
+                },
+            ]
+                .concat(
+                    keywords.length > 0
+                        ? {
+                              name: `keywords`,
+                              content: keywords.join(`, `),
+                          }
+                        : []
+                )
+                .concat(meta)}
+        />
+    );
+};
 
 SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  keywords: [],
-  description: ``,
-}
+    lang: `en`,
+    meta: [],
+    keywords: [],
+    description: ``,
+};
 
 SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  keywords: PropTypes.arrayOf(PropTypes.string),
-  title: PropTypes.string,
-}
+    description: PropTypes.string,
+    lang: PropTypes.string,
+    meta: PropTypes.arrayOf(PropTypes.object),
+    keywords: PropTypes.arrayOf(PropTypes.string),
+    title: PropTypes.string,
+};
 
-export default SEO
+export default SEO;
