b4c3bf1a18b2398bdc58736ba4a8361110ebefdb
AndreBClark <andre@cosmicdivision.dev>
2020-02-25 21:23:54 -0800

    :sparkles: implement animated more info modal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,25 +9,27 @@ import OutsideClickHandler from 'react-outside-click-handler/build/OutsideClickH
 const Header = () => {
     const [infobtn, infoClicked] = useState(false);
     return (
-        <header class="text-teal-400 w-full p-4 xs:relative">
+        <header class={`${infobtn ? 'overlay' : ''}`}>
             <h1 class="h2 md:text-5xl text-center font-black xl:mb-3">
                 Companion Dice
                 <button
+                    aria-label="more info button"
                     class="sm:hidden p-2"
                     onClick={() => infoClicked(!infobtn)}>
                     <InfoSVG />
                 </button>
             </h1>
-
             <OutsideClickHandler onOutsideClick={() => infoClicked(false)}>
-                <div class={`modal ${infobtn ? 'modal--active' : 'hidden'}`}>
+                <div class={`modal${infobtn ? ' modal--active' : ''}`}>
                     <a href="https://cosmicdivision.dev">
                         <CosmicSVG />
                         Andre Clark
+                        <p>Developer</p>
                     </a>
                     <a href="https://nathangoullette.com">
                         <NateSVG />
                         Nathan Goullette
+                        <p>Designer</p>
                     </a>
                     <a href="https://github.com/AndreBClark/companiondice">
                         <GithubSVG />
