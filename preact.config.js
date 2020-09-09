7b014c7998486db85577f319d36b11957a398664
AndreBClark <andre@cosmicdivision.dev>
2020-09-09 14:09:52 -0700

    :package: :add preact-cli-plugin-netlify

--- preact.config.js
+++ preact.config.js
@@ -1,7 +1,8 @@
 const preactCliSvgLoader = require('preact-cli-svg-loader');
 const preactCliPostCSS = require("preact-cli-postcss");
-
+const netlifyPlugin = require('preact-cli-plugin-netlify');
 export default function (config, env, helpers) {
   preactCliSvgLoader(config, helpers);
   preactCliPostCSS(config, helpers);
+  netlifyPlugin(config);
 }
