03325e3e8a10636862a2c2b6a54bb028c0d22ca6
andrebclark <andre@cosmicdivision.dev>
2021-01-12 22:07:24 -0800

    basetext: remove selectability

--- src/components/BaseText.jsx
+++ src/components/BaseText.jsx
@@ -5,7 +5,8 @@ import { tailwind } from './tailwind'
 const BaseText = props => {
   return (
     <Text {...props}
-      style={[tailwind('text-green-400 font-semibold text-center'), props.style]}>
+      selectable={false}
+      style={[tailwind(`text-green-400 font-semibold text-center`), props.style]}>
       {props.children}
     </Text>
   )
