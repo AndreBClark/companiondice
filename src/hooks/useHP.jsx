746682b9731cc2d87894bfb9cbbb9b266caadc5f
andrebclark <andre@cosmicdivision.dev>
2021-01-13 01:13:36 -0800

    HPtracker: validate number values and fix uncontrolled/controlled input error

--- src/hooks/useHP.jsx
+++ src/hooks/useHP.jsx
@@ -3,10 +3,15 @@ export function useHP() {
   const [hitpoints, setHitpoints] = useAsyncStorage('currentHP', 0);
   const [maxHP, setMaxHP] = useAsyncStorage('maxHP', 0);
   const modifyHP = mod => {
-    setHitpoints(Number(hitpoints + mod));
+    const newHP = Number(hitpoints + mod);
+    const isBelowMax = newHP <= maxHP;
+    const isPositive = newHP >= 0;
+    const validatedValue = isBelowMax ? isPositive ? newHP : 0 : maxHP
+    setHitpoints(validatedValue);
   };
   const longRest = () => {
     setHitpoints(Number(maxHP));
+    setMaxHP(Number(maxHP))
   };
   return { maxHP, setMaxHP, hitpoints, setHitpoints, modifyHP, longRest };
 }
--- src/routes/health/index.jsx
+++ src/routes/health/index.jsx
@@ -5,70 +5,73 @@ import { View, TextInput, StyleSheet } from 'react-native'
 import { tailwind } from '../../components/tailwind';
 import BaseText from '../../components/BaseText';
 import brandColor from '../../components/brandColor';
+
+
+
 const HealthTracker = () => {
   const {
-    hitpoints,
+    hitpoints = 0,
     setHitpoints,
-    maxHP,
+    maxHP = 0,
     setMaxHP,
     modifyHP,
     longRest,
   } = useHP();
-
   return (
-    <View style={tw.container}>
-      <HPButton onPress={longRest} Label="Long Rest" />
-      <View>
-        <BaseText>Heal</BaseText>
-        <View style={tw.col}>
-          <HPButton onPress={() => modifyHP(1)} Label="+" />
-          <HPButton onPress={() => modifyHP(5)} Label="5" />
-          <HPButton onPress={() => modifyHP(10)} Label="10" />
+        <View style={tw.container}>
+          <HPButton onPress={longRest} Label="Long Rest" />
+          <View>
+            <BaseText>Heal</BaseText>
+            <View style={tw.col}>
+              <HPButton onPress={() => modifyHP(1)} Label="+" />
+              <HPButton onPress={() => modifyHP(5)} Label="5" />
+              <HPButton onPress={() => modifyHP(10)} Label="10" />
+            </View>
+          </View>
+          <View className="grid grid-cols-3 col-gap-4 gap-4 mt-4">
+            <View className="flex flex-col justify-center font-black bg-purple-700 rounded-lg">
+              <BaseText className="text-2xl font-bold">Current</BaseText>
+              <TextInput
+                selectionColor={brandColor}
+                keyboardType="numeric"
+                keyboardAppearance="dark"
+                textContentType="none"
+                textAlign="center"
+                placeholder="--"
+                maxLength={3}
+                onChangeText={setHitpoints}
+                value={hitpoints}
+                style={tw.input}
+                />
+            </View>
+            <BaseText style={tw.slash}>/</BaseText>
+            <View className="flex flex-col justify-center font-black bg-purple-700 rounded-lg">
+              <BaseText className="text-2xl font-bold">Max</BaseText>
+                <TextInput
+                  selectionColor={brandColor}
+                  keyboardType="numeric"
+                  keyboardAppearance="dark"
+                  textContentType="none"
+                  textAlign="center"
+                  placeholder="--"
+                  maxLength={3}
+                  onChangeText={setMaxHP}
+                  value={maxHP}
+                  style={tw.input}
+                />
+            </View>
+          </View>
+          <View>
+            <BaseText>Damage</BaseText>
+            <View className="grid grid-cols-3 gap-4 col-gap-4 ">
+              <HPButton onPress={() => modifyHP(-1)} Label="-" />
+              <HPButton onPress={() => modifyHP(-5)} Label="5" />
+              <HPButton onPress={() => modifyHP(-10)} Label="10" />
+            </View>
+          </View>
         </View>
-      </View>
-      <View className="grid grid-cols-3 col-gap-4 gap-4 mt-4">
-        <View className="flex flex-col justify-center font-black bg-purple-700 rounded-lg">
-          <BaseText className="text-2xl font-bold">Current</BaseText>
-          <TextInput
-            selectionColor={brandColor}
-            keyboardType="numeric"
-            keyboardAppearance="dark"
-            textContentType="none"
-            textAlign="center"
-            placeholder="--"
-            maxLength={3}
-            onChangeText={setHitpoints}
-            value={hitpoints}
-            defaultValue={24}
-            style={tw.input}
-            />
-        </View>
-        <BaseText style={tw.slash}>/</BaseText>
-        <View className="flex flex-col justify-center font-black bg-purple-700 rounded-lg">
-          <BaseText className="text-2xl font-bold">Max</BaseText>
-            <TextInput
-              selectionColor={brandColor}
-              keyboardType="numeric"
-              keyboardAppearance="dark"
-              textContentType="none"
-              textAlign="center"
-              placeholder="--"
-              maxLength={3}
-              onChangeText={setMaxHP}
-              value={maxHP}
-              style={tw.input}
-            />
-        </View>
-      </View>
-      <View>
-        <BaseText>Damage</BaseText>
-        <View className="grid grid-cols-3 gap-4 col-gap-4 ">
-          <HPButton onPress={() => modifyHP(-1)} Label="-" />
-          <HPButton onPress={() => modifyHP(-5)} Label="5" />
-          <HPButton onPress={() => modifyHP(-10)} Label="10" />
-        </View>
-      </View>
-    </View>
+      //   )}
+      // </Formik>
   );
 };
 const tw = StyleSheet.create({
