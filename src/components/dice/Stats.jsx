ebf2d7fa198b985f2782c60e3bc17af82ed21f43
andre clark <andre@cosmicdivision.dev>
2021-01-14 21:58:53 -0800

    center dice indicator

--- src/components/dice/Stats.jsx
+++ src/components/dice/Stats.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { useSpring, animated, config } from 'react-spring/native';
+import { useSpring, animated, config } from 'react-spring';
+import { Pressable, StyleSheet, View, Animated } from 'react-native';
 import { useRoll } from '../../hooks/diceHelpers';
-import { Pressable, StyleSheet, View } from 'react-native';
 import { tailwind } from '../tailwind'
 import TailwindText from '../TailwindText';
 const AnimatedView = animated(View);
@@ -10,23 +10,21 @@ const Dicebox = props => {
 
   const Spin = useSpring({
     config: config.wobbly,
-    transform: isActive ? 'rotate(-30deg)' : 'rotate(0turn)',
+    transform: isActive ? 'rotate(-90deg)' : 'rotate(0turn)',
   });
-  function handleDiceRoll() {
-    return rollDice(6, 18);
-  }
+  const handleDiceRoll = () => rollDice(6, 18);
   return (
     <Pressable
       onPress={handleDiceRoll}
-      style={tw.dice}>
+      style={[Spin, tw.dice]}>
       <TailwindText 
         style={tw.number} 
         size="7xl" 
-        color="purple-800">{
-        result}
+        color="purple-800">
+        {result}
       </TailwindText>
       <AnimatedView 
-        style={[tw.svgWrapper, Spin]}>
+        style={[tw.svgWrapper]}>
         {props.children}
       </AnimatedView>
     </Pressable>
@@ -34,8 +32,8 @@ const Dicebox = props => {
 };
 
 const tw = StyleSheet.create({
-  dice: tailwind(`flex font-bold mb-3 relative mx-auto w-84 h-full items-center justify-content`),
-  number: tailwind(`relative mx-auto bg-green-400 rounded-full justify-center items-center`),
+  dice: tailwind(`flex font-bold mb-3 relative mx-auto w-84 h-full items-center justify-center`),
+  number: tailwind(`mx-auto bg-green-400 rounded-full justify-center m-auto items-center z-10`),
   svgWrapper: tailwind(`w-full h-full absolute top-0 left-0 justify-center items-center`)
 })
 
--- src/routes/dice/index.jsx
+++ src/routes/dice/index.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
+import { View, StyleSheet } from 'react-native';
 import Dicebox from '../../components/dice/dicebox';
 import Controls from '../../components/Controls';
 import { useRoll, diceContext } from '../../hooks/diceHelpers';
 import { D4, D6, D8, D10, D12, D20, D100 } from '../../components/Svg';
 import RollStats from '../../components/dice/Stats';
+import { tailwind } from '../../components/tailwind';
 const Dice = props => {
   const { setSides, sides } = useRoll();
   const [amount, updateAmount] = useState(1);
@@ -12,27 +14,18 @@ const Dice = props => {
   const reset = () => updateAmount(1);
   return (
     <diceContext.Provider value={{ amount, increment, decrement, reset }}>
-      {/* <Router> */}
-        {/* <Dicebox default sides={sides}>
-          <Router>
-            <D4 path="dice/4" />
-            <D6 path="dice/6" />
-            <D8 path="dice/8" />
-            <D10 path="dice/10" />
-            <D12 path="dice/12" />
-            <D20 path="dice/20" />
-            <D100 path="dice/100" />
-          </Router>
-        </Dicebox> */}
-      <RollStats
-        // path="dice/stats" 
-        sides={20}>
-        <D6 size="100%"/>
-      </RollStats> 
-      {/* </Router> */}
-      {/* <Controls sides={sides} /> */}
+      <View style={tw.dicebox}>
+        <RollStats
+          sides={20}>
+          <D6 />
+        </RollStats> 
+      </View>
     </diceContext.Provider>
   );
 };
 
 export default Dice;
+
+const tw = StyleSheet.create({
+  dicebox: tailwind(`justify-center h-full`)
+})
