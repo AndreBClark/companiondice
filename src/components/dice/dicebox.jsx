8bef8f9083983c196974a8a4814de8e9d5013781
andre clark <andre@cosmicdivision.dev>
2021-01-17 17:44:21 -0800

    Update dicebox.jsx

--- src/components/dice/dicebox.jsx
+++ src/components/dice/dicebox.jsx
@@ -1,45 +1,36 @@
 import React, { useContext } from 'react';
-import { useSpring, animated, config } from 'react-spring/native';
+import { useSpring, animated } from 'react-spring';
 import { Pressable, StyleSheet, View } from 'react-native';
 import { useRoll, diceContext } from '../../hooks/diceHelpers';
 import { tailwind } from '../tailwind';
 import TailwindText from '../TailwindText';
 import { isWeb } from '../Constants';
-import { D6 } from '../Svg';
+import { D6, D20 } from '../Svg';
 
-const SpinTransform = {
-  transform: {
-    rotateX: isActive ? '90deg': '0deg',
-  },
-};
-const SvgWrapper = props => {
-  return(
-    <View style={[Spin, tw.svgWrapper]}>{props.children}</View>
-  )
-}
-
-const AnimatedView = animated(SvgWrapper);
+const AnimatedView = animated(View);
 
 export const Dice = props => {
-  const { result, rollDice, isActive, sides, setSides } = useRoll();
+  const { result, rollDice, isActive } = useRoll();
   const { amount } = useContext(diceContext);
   const handleDiceRoll = () => rollDice();
 
-  const Spin = useSpring(SpinTransform);
   return (
-    <Pressable onPress={handleDiceRoll} style={tw.dice}>
-      <TailwindText style={tw.number} size="7xl" color="purple-800">
-        {result}
-      </TailwindText>
-      <AnimatedView style={Sping}>{props.children}</AnimatedView>
-    </Pressable>
+    <View style={tw.dicebox}>
+      <Pressable onPress={handleDiceRoll} style={tw.dice}>
+        <TailwindText style={tw.number} size="7xl" color="purple-800">
+          {result}
+        </TailwindText>
+        <AnimatedView
+          style={tw.svgWrapper}>
+          <D20 size="100%" />
+        </AnimatedView>
+      </Pressable>
+    </View>
   );
 };
 
 export const Stats = props => {
   const { result, rollDice, isActive } = useRoll();
-  // console.log({ isActive })
-  const Spin = useSpring(SpinTransform);
   const handleDiceRoll = () => rollDice(6, 18);
   return (
     <View style={tw.dicebox}>
@@ -47,8 +38,9 @@ export const Stats = props => {
         <TailwindText style={tw.number} size="7xl" color="purple-800">
           {result}
         </TailwindText>
-        <AnimatedView style={isActive && Spin}>
-          <D6 />
+        <AnimatedView
+          style={tw.svgWrapper}>
+          <D6 size="100%" />
         </AnimatedView>
       </Pressable>
     </View>
@@ -57,14 +49,27 @@ export const Stats = props => {
 
 const tw = StyleSheet.create({
   dice: tailwind(
-    `flex font-bold mb-3 relative mx-auto w-64 h-64 items-center justify-center relative`
+    `flex font-bold mb-3 relative m-auto w-64 h-64 items-center justify-center relative`
   ),
   number: tailwind(
-    `bg-green-400 rounded-full justify-center m-auto items-center z-10`
+    `bg-green-400 rounded-full justify-center m-auto items-center justify-center z-10`
   ),
   svgWrapper: tailwind(
-    `w-full h-full absolute top-0 left-0 justify-center items-center ${
-      isWeb ? 'transition-all' : ''
-    }`
+    `w-full h-full absolute top-0 left-0 justify-center items-center ${isWeb ? 'transition-all' : ''}`
   ),
+  dicebox: tailwind(`justify-center h-full`),
 });
+
+const SpinTransform = {
+  from: {
+    transform: {
+      rotateX: '0deg',
+    }
+  },
+  to: {
+    transform: {
+      rotateX: '30deg'
+    }
+  }
+
+};
