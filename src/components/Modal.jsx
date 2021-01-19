9c0627e8f928259ecbf80329896dc5b5ef5e3093
andre clark <andre@cosmicdivision.dev>
2021-01-18 18:46:35 -0800

    fix: Modal on web
    remove need for web modal component by updating react native web to 14.10

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import Modal from 'react-native-modal';
-import ModalWeb from 'modal-enhanced-react-native-web';
-import { isWeb } from '../components/Constants';
+// import ModalWeb from 'modal-enhanced-react-native-web';
+// import { isWeb } from '../components/Constants';
 
 const ModalCrossPlatform = props =>
-  isWeb ? (
-    <ModalWeb {...props}>{props.children}</ModalWeb>
-  ) : (
     <Modal {...props}>{props.children}</Modal>
-  );
 
 export default ModalCrossPlatform;
