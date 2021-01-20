8f4d1d8bd7251934d0c842053185dfeb310d2a63
andrebclark <andre@cosmicdivision.dev>
2021-01-19 21:52:52 -0800

    Update dicebox.jsx

--- src/components/dice/dicebox.jsx
+++ src/components/dice/dicebox.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring/native';
 import { Pressable, StyleSheet, View, useWindowDimensions } from 'react-native';
 import { useRoll } from '../../hooks/diceHelpers';
 import { tailwind } from '../tailwind';
