8cf9c7fc647ed1d8f2c409381bba5d3f3d56fdf5
andre clark <andre@cosmicdivision.dev>
2021-01-23 02:22:19 -0800

    module resolver for shorter imports

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,17 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        require.resolve('babel-plugin-module-resolver'),
+        {
+          root: ["./src/"],
+          alias: {
+            "@components": "./src/components",
+            "@hooks": "./src/hooks"
+          }
+        }
+      ]
+    ]
   };
 };
